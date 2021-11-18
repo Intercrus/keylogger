b49cf70d2c8358db04520f313835e0950383a1e0
Intercrus <42522221+Intercrus@users.noreply.github.com>
2021-11-18 04:10:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ environment.read_env()
 SEND_REPORT_EVERY = environment.int("SEND_REPORT_EVERY")
 TELEGRAM_BOT_TOKEN = environment.str("TELEGRAM_BOT_TOKEN")
 ADMIN_CHAT_ID = environment.str("ADMIN_CHAT_ID")
+
 EMAIL = environment.str("EMAIL")
 PASSWORD = environment.str("PASSWORD")
 
